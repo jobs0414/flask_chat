65a2a7a710648dda720fc7052367a17f04e28336
Han Sang Woo <tkddn262@naver.com>
2018-08-30 02:32:41 +0000

    chatbot

--- README.md
+++ README.md
@@ -37,8 +37,21 @@ def keyboard():
 
 
 app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
-'''
 
 
+
+
+### API 
+
+-request 
+    -url : 어떤 경로로 보낼꺼니? 
+    -method : 어떤 방법으로 보낼꺼니? 
+    -parameter : 어떤 정보를 담을꺼니? 
+    
+
+    
+-response
+    - data type : 어떤 형식으로 답할까? 
+    
 
 
--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 #json으로 바꾸어주기 위한 라이브러리 추가 
 import json
-from flask import Flask
+from flask import Flask, request ,jsonify
 
 app=Flask(__name__)
 
@@ -21,6 +21,28 @@ def keyboard():
     #딕셔너리를 json으로 바꿔서 리턴 해주기 위한 코드 
     json_keyboard= json.dumps(keyboard)
     return json_keyboard
+    
+    
+@app.route('/message',methods=['POST'])
+def message(): 
+    
+    # content라는 key의 value를 msg에 저장 
+    msg= request.json['content']
+
+    json_return = {
+        "message":{
+            "text": msg    
+            
+        },
+        
+        "keyboard":  {
+            "type" : "buttons",
+            "buttons" : ["메뉴", "로또", "고양이","영화"]
+          }
+        
+    }
+    
+    return jsonify(json_return)
 
 
 app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
